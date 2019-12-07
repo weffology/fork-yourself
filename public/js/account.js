29a57533ec3a7329392766ee38e59958ff6f6901
weffology <stephlarkinrose@gmail.com>
2019-12-07 00:39:56 -0500

    changes to account.js to simplify

--- public/js/account.js
+++ public/js/account.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var user_recipes;
-  var userString;
+  // var userString;
 
   $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
@@ -11,38 +11,47 @@ $(document).ready(function () {
   
   $.get("/api/recipes").then(function (data) {
     console.log(data);
-  });
-
-  showFavorites();
-
-  function showFavorites(recipename) {
-    userString = recipename || "";
-    $.get("/api/recipes", function (data) {
-      user_recipes = data;
-      initializeRows()
-    });
-  };
-
-  function initializeRows() {
     var postsToAdd = [];
-    for (var i = 0; i < user_recipes.length; i++) {
-      postsToAdd.push(createNewRow(user_recipes[i]));
-    }
-  };
-
-  function createNewRow(recipe) {
+    for (var i = 0; i < data.length; i++) {
+      postsToAdd.push(createNewRow(data[i]));
+    };
     var newRecipeURL = recipe.recipe_URL.replace(/\/$/, "");
     var divRecipes = $("<div>");
     divRecipes.attr("class", "recipe");
     divRecipes.append("<p><a href=" + newRecipeURL + '>' + recipe.recipe_name + "</a></p>");
-    $("#recipe_name").append(divRecipes);
-    // return divRecipes;
-  }
+    $("#savedRecipes").append(divRecipes);
+  });
 
-  function noRecipes() {
-    console.log("Somethings forked up. Please try again")
-    blogContainer.append("<p>You Forked Up Again</p>");
-  }
+  // showFavorites();
+
+  // function showFavorites(recipename) {
+  //   userString = recipename || "";
+  //   $.get("/api/recipes", function (data) {
+  //     user_recipes = data;
+  //     initializeRows()
+  //   });
+  // };
+
+  // function initializeRows() {
+  //   var postsToAdd = [];
+  //   for (var i = 0; i < user_recipes.length; i++) {
+  //     postsToAdd.push(createNewRow(user_recipes[i]));
+  //   }
+  // };
+
+  // function createNewRow(recipe) {
+  //   var newRecipeURL = recipe.recipe_URL.replace(/\/$/, "");
+  //   var divRecipes = $("<div>");
+  //   divRecipes.attr("class", "recipe");
+  //   divRecipes.append("<p><a href=" + newRecipeURL + '>' + recipe.recipe_name + "</a></p>");
+  //   $("#recipe_name").append(divRecipes);
+  //   // return divRecipes;
+  // }
+
+  // function noRecipes() {
+  //   console.log("Somethings forked up. Please try again")
+  //   blogContainer.append("<p>You Forked Up Again</p>");
+  // }
 
 
 
